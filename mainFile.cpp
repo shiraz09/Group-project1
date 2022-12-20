b47303e9e87fd20a56e06a5ccbe94ef9abcb4178
Shiraz <Shiraz120010@gmail.com>
2022-12-20 15:55:36 +0200

    init worker function and worker file

--- mainFile.cpp
+++ mainFile.cpp
@@ -19,7 +19,7 @@
 #include <thread>
 
 using namespace std::this_thread;     // sleep_for, sleep_until
-using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+using namespace std::chrono_literals ; // ns, us, ms, s, h, etc.
 using std::chrono::system_clock ;
 
 /*
